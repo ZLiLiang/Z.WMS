62c94845094e5f72ae5909478f78e57e94a2706c
LL zhang <zhang31666@outlook.com>
2023-09-09 10:49:59 +0800

    封装ado

---
+++ Z.DbUtility/CommandInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+
+namespace Z.DbUtility
+{
+    public class CommandInfo
+    {
+        public string CommandText; //sql或存储过程名
+        public SqlParameter[] Paras; //参数列表
+        public bool IsProc; //是否是存储过程
+
+        public CommandInfo()
+        {
+        }
+
+        public CommandInfo(string commandText, bool isProc)
+        {
+            CommandText = commandText;
+            IsProc = isProc;
+        }
+
+        public CommandInfo(string commandText, SqlParameter[] paras, bool isProc)
+        {
+            CommandText = commandText;
+            Paras = paras;
+            IsProc = isProc;
+        }
+    }
+}
