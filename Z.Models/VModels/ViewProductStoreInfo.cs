7374659a1235f0f5d61ce6ba5365e49a62cd3758
LL zhang <zhang31666@outlook.com>
2023-09-21 16:59:25 +0800

    添加行特性

--- Z.Models/VModels/ViewProductStoreInfo.cs
+++ Z.Models/VModels/ViewProductStoreInfo.cs
@@ -6,13 +6,28 @@ namespace Z.Models.VModels
     [PrimaryKey("ProStoreId")]
     public class ViewProductStoreInfo
     {
+        [Column("产品仓库标识")]
         public int ProStoreId { get; set; }
+
+        [Column("产品标识")]
         public int ProductId { get; set; }
+
+        [Column("产品名称")]
         public string ProductName { get; set; }
+
+        [Column("仓库标识")]
         public int StoreId { get; set; }
+
+        [Column("仓库名称")]
         public string StoreName { get; set; }
+
+        [Column("仓库区域编号")]
         public int SRegionId { get; set; }
+
+        [Column("仓库区域名称")]
         public string SRegionName { get; set; }
+
+        [Column("商品数量")]
         public int ProductCount { get; set; }
         public int IsDeleted { get; set; }
     }
